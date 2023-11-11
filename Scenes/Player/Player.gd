af5bf7addc9b2bfb2f2b36d5861a63dad614f0bf
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-11-11 17:55:10 +0100

    Changed Player Script for better gravity and added global variables to change specific actions

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,8 +1,16 @@
 extends KinematicBody2D;
 
-var speed = 200;
+export(int) var JUMP_FORCE_MAX = 200;
+export(int) var FRICTION = 10;
+export(int) var GRAVITY = 5;
+export(int) var ADDITIONAL_GRAVITY = 3;
+
 var velocity = Vector2.ZERO;
+
 var mouse_position = null;
+var direction;
+
+var fast_fell = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,25 +18,25 @@ func _ready():
 
 func _physics_process(delta):
 	apply_gravity();
-	var input = Vector2.ZERO;
 	mouse_position = get_global_mouse_position();
 	
-	if Input.is_mouse_button_pressed(1) and self.is_on_floor():
-		var direction = (mouse_position - position).normalized();
-		velocity = direction * speed;
+	if is_on_floor():
+		if Input.is_mouse_button_pressed(1):
+			direction = (mouse_position - position).normalized();
+			velocity = direction * JUMP_FORCE_MAX;
+	else:
+		if velocity.y > 0:
+			velocity.y += ADDITIONAL_GRAVITY;
 	
-	if !Input.is_mouse_button_pressed(1) && self.is_on_floor():
-		velocity.x = 0;
+	if !Input.is_mouse_button_pressed(1) and is_on_floor():
+		apply_friction();
 	
 	
 	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP);
 	#look_at(mouse_position);
-		
 
 func apply_gravity():
-	velocity.y += 4;
-	
+	velocity.y += GRAVITY;
+
 func apply_friction():
-	pass;
-	
- 
+	velocity.x = move_toward(velocity.x, 0, FRICTION);
