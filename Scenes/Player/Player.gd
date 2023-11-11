d482733341c40b6fb1d5f5bc4cca1266d6b5d45e
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-11-11 18:43:14 +0100

    Fixed Godot 4 compatibility

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,11 +1,9 @@
-extends KinematicBody2D;
+extends CharacterBody2D;
 
-export(int) var JUMP_FORCE_MAX = 200;
-export(int) var FRICTION = 10;
-export(int) var GRAVITY = 5;
-export(int) var ADDITIONAL_GRAVITY = 3;
-
-var velocity = Vector2.ZERO;
+@export var JUMP_FORCE_MAX: int = 200;
+@export var FRICTION: int = 10;
+@export var GRAVITY: int = 5;
+@export var ADDITIONAL_GRAVITY: int = 3;
 
 var mouse_position = null;
 var direction;
@@ -28,11 +26,18 @@ func _physics_process(delta):
 		if velocity.y > 0:
 			velocity.y += ADDITIONAL_GRAVITY;
 	
-	if !Input.is_mouse_button_pressed(1) and is_on_floor():
+	if is_on_floor():
 		apply_friction();
 	
+	if Input.is_mouse_button_pressed(2):
+		apply_zero_friction();
 	
-	velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP);
+	
+	set_velocity(velocity)
+	# TODOConverter3To4 looks that snap in Godot 4 is float, not vector like in Godot 3 - previous value `Vector2.DOWN`
+	set_up_direction(Vector2.UP)
+	move_and_slide()
+	velocity = velocity;
 	#look_at(mouse_position);
 
 func apply_gravity():
@@ -40,3 +45,6 @@ func apply_gravity():
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, FRICTION);
+
+func apply_zero_friction():
+	velocity.x = move_toward(velocity.x, 10, 0);
