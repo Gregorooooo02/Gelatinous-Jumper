0edc5a2727d3790800b765f3c181c1535d4b88d7
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-11-13 20:41:51 +0100

    Added wall jumping, still trying to fix it

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Basic, 2D Platformer, whhere you control player with just a mouse. Easy, right?
 - [X] Player Controls (jumping, rolling, wall sliding, etc.)
 - [X] Player Animations (creating new Sprite and Animations for different states of the slime)
 - [ ] Player Damage and Respawn System
-- [ ] Build a basic Level for presentation
+- [X] Build a basic Level for presentation
 - [ ] Dangers for the slime
 - [ ] (Optional) Create Second level and Transitions to next levels
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D;
 
-@export var JUMP_FORCE_MAX: int = 200;
+@export var JUMP_FORCE_MAX: int = 250;
 @export var FRICTION: int = 10;
-@export var GRAVITY: int = 5;
+@export var GRAVITY: int = 10;
 @export var ADDITIONAL_GRAVITY: int = 3;
+@export var WALL_SLIDING_GRAVITY = 5;
 
 var mouse_position = null;
 var direction;
+var collide;
 
 var hold_right_button = false;
 var counter = 1;
+
+var is_wall_sliding = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,55 +23,52 @@ func _physics_process(delta):
 	apply_gravity();
 	mouse_position = get_global_mouse_position();
 	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		hold_right_button = true;
+		$AnimatedSprite2D.animation = "Rolling";
+	else:
+		hold_right_button = false;
+		$AnimatedSprite2D.animation = "Idle";
+	
 	if is_on_floor():
-		if Input.is_mouse_button_pressed(1):
-			direction = (mouse_position - position).normalized();
-			velocity = direction * JUMP_FORCE_MAX;
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			jumping_movement();
+		elif hold_right_button:
+			apply_zero_friction();
+		else:
+			apply_friction();
 	else:
 		if velocity.y > 0:
 			velocity.y += ADDITIONAL_GRAVITY;
 	
-	if is_on_floor():
-		if hold_right_button:
-			apply_zero_friction();
-			ball_bounce_of_floor(counter);
-			counter += 1;
-		else:
-			apply_friction();
-			counter = 0;
-			
-	if is_on_wall():
-		if hold_right_button:
-			ball_bounce_of_wall(counter);
-			counter += 1;
+	if is_on_wall() and !Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		is_wall_sliding = true;
+		apply_wall_slide();
+	else:
+		is_wall_sliding = false;
 	
-	if Input.is_action_pressed("hold_right_button"):
-		hold_right_button = true;
-		$AnimatedSprite2D.animation = "Rolling";
-	elif Input.is_action_just_released("hold_right_button"):
-		hold_right_button = false;
-		$AnimatedSprite2D.animation = "Idle";
-	
-	
-	set_velocity(velocity)
-	# TODOConverter3To4 looks that snap in Godot 4 is float, not vector like in Godot 3 - previous value `Vector2.DOWN`
-	set_up_direction(Vector2.UP)
-	move_and_slide()
-	velocity = velocity;
 	#look_at(mouse_position);
+	move_and_slide();
+
+func jumping_movement():
+	direction = global_position.direction_to(mouse_position);
+	velocity = direction * JUMP_FORCE_MAX;
 
 func apply_gravity():
 	velocity.y += GRAVITY;
-	velocity.y = min(velocity.y, 300);
+	velocity.y = min(velocity.y, 200);
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, FRICTION);
 
 func apply_zero_friction():
 	velocity.x = move_toward(velocity.x, 10, 0);
-	
-func ball_bounce_of_floor(counter):
-	velocity.y = move_toward(-JUMP_FORCE_MAX, 0, 60 * counter);
-	
-func ball_bounce_of_wall(counter):
-	velocity.x = move_toward(- JUMP_FORCE_MAX, 0, 60 * counter);
+
+func apply_wall_slide():
+	if is_wall_sliding:
+		velocity.y += WALL_SLIDING_GRAVITY;
+		velocity.y = min(velocity.y, WALL_SLIDING_GRAVITY * 1.5);
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			jumping_movement();
+	else:
+		pass;
