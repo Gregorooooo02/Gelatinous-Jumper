ac3e52398c6edb53bd9dc06fa86f46b7b4ee057d
Grzegorz Bednarek <106348157+Gregorooooo02@users.noreply.github.com>
2023-11-14 15:30:17 +0100

    Added charge jump - works

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,10 +1,15 @@
 extends CharacterBody2D;
 
-@export var JUMP_FORCE_MAX: int = 220;
-@export var FRICTION: int = 10;
-@export var GRAVITY: int = 10;
-@export var ADDITIONAL_GRAVITY: int = 3;
-@export var WALL_SLIDING_GRAVITY: int = 6;
+const JUMP_FORCE_MAX: int = 250;
+const JUMP_TIME_START: float = 0.2;
+const JUMP_TIME_MAX: float = 1.0;
+const FRICTION: int = 10;
+const GRAVITY: int = 10;
+const ADDITIONAL_GRAVITY: int = 3;
+const WALL_SLIDING_GRAVITY: int = 6;
+
+var is_charging_jump = false;
+var current_jump_time = JUMP_TIME_START;
 
 var is_mouse_in_box = false;
 var mouse_position = null;
@@ -31,16 +36,22 @@ func _physics_process(delta):
 	
 	if is_mouse_in_box:
 		if is_on_floor():
-			if Input.is_action_just_pressed("left_click"):
+			if Input.is_action_pressed("left_click"):
+				is_charging_jump = true;
+				if is_charging_jump:
+					current_jump_time += delta * 2;
+					current_jump_time = clamp(current_jump_time, JUMP_TIME_START, JUMP_TIME_MAX);
+			if Input.is_action_just_released("left_click"):
+				is_charging_jump = false;
 				jumping_movement();
-			elif hold_right_button:
+			if hold_right_button:
 				apply_zero_friction();
 				if velocity.x > 0:
 					$AnimatedSprite2D.rotate(0.25);
 				elif velocity.x < 0:
 					$AnimatedSprite2D.rotate(-0.25);
 			else:
-				apply_friction();
+				apply_friction();	
 				$AnimatedSprite2D.rotation = 0;
 		else:
 			if velocity.y > 0:
@@ -49,7 +60,8 @@ func _physics_process(delta):
 	if is_on_wall() and !Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		is_wall_sliding = true;
 		$CollisionShape2D.shape.extents = Vector2(8, 4);
-		apply_wall_slide();
+		$AnimatedSprite2D.rotation = 0;
+		apply_wall_slide(delta);
 	else:
 		is_wall_sliding = false;
 		$CollisionShape2D.shape.extents = Vector2(8, 8);
@@ -66,7 +78,8 @@ func _physics_process(delta):
 
 func jumping_movement():
 	direction = global_position.direction_to(mouse_position);
-	velocity = direction * JUMP_FORCE_MAX;
+	velocity = direction * JUMP_FORCE_MAX * (current_jump_time / JUMP_TIME_MAX);
+	current_jump_time = JUMP_TIME_START;
 
 func apply_gravity():
 	velocity.y += GRAVITY;
@@ -78,14 +91,18 @@ func apply_friction():
 func apply_zero_friction():
 	velocity.x = move_toward(velocity.x, 10, 0);
 
-func apply_wall_slide():
+func apply_wall_slide(delta):
 	if is_wall_sliding:
 		velocity.y += WALL_SLIDING_GRAVITY;
 		velocity.y = min(velocity.y, WALL_SLIDING_GRAVITY * 3);
-		if Input.is_action_just_pressed("left_click"):
+		if Input.is_action_pressed("left_click"):
+			is_charging_jump = true;
+			if is_charging_jump:
+				current_jump_time += delta * 2;
+				current_jump_time = clamp(current_jump_time, JUMP_TIME_START, JUMP_TIME_MAX);
+		if Input.is_action_just_released("left_click"):
+			is_charging_jump = false;
 			jumping_movement();
-	else:
-		pass;
 
 func _on_area_2d_mouse_entered():
 	is_mouse_in_box = true;
