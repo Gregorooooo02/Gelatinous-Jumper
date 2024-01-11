27117b74bdc1d7760a8a7f9509b4bb5c3568d5f4
Wiktoryk <242486@edu.p.lodz.pl>
2024-01-11 01:14:54 +0100

    ekran końcowy wyświetlał się natychmiast

--- Scenes/Levels/Screens/end_box.gd
+++ Scenes/Levels/Screens/end_box.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
 func _on_body_entered(body):
-	get_tree().change_scene_to_file("res://Scenes/Levels/Screens/End_Screen.tscn");
-	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE);
+	if body is Player:
+		get_tree().change_scene_to_file("res://Scenes/Levels/Screens/End_Screen.tscn");
+		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE);
