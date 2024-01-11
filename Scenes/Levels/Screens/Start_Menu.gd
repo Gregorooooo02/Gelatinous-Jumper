6813158bd71585e424156e74e970c6b9a52bc262
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-11 10:54:41 +0100

    Fixed sizing of the menu

--- Scenes/Levels/Screens/Start_Menu.gd
+++ Scenes/Levels/Screens/Start_Menu.gd
@@ -3,6 +3,9 @@ extends Node
 func _ready():
 	$VBoxContainer/VBoxContainer/Start.grab_focus();
 	$AnimationPlayer.play("Start");
+	
+	$VBoxContainer.size = Vector2(480, 270);
+	$VBoxContainer.custom_minimum_size = Vector2(480, 270);
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/Level1/Level_1.tscn");
