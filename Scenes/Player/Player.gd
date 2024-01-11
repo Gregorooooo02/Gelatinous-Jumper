45e2501034638c6339652470a9af197faf588559
Jakub Łysiuk <242457@edu.p.lodz.pl>
2024-01-11 22:26:05 +0100

    Dash particles + sprite + animacje
    pod zadnym pozorem nie jedzcie niebieskiej makareny

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -11,13 +11,20 @@ class_name Player;
 @onready var animation_player = $AnimationPlayer;
 @onready var sprite = $MainBody;
 @onready var ball_sprite = $BallBody;
+@onready var dash_sprite = $DashBody;
+@onready var dash_sprite_ball = $DashBallBody;
 @onready var sprite_eyes_body = $MainBody/PlayerEyes;
 @onready var sprite_eyes_ball = $BallBody/PlayerEyes;
+@onready var sprite_eyes_dash = $DashBody/PlayerEyes;
+@onready var sprite_eyes_dash_ball = $DashBallBody/PlayerEyes;
+
 @onready var base_scale: Vector2 = sprite.scale;
 @onready var base_ball_scale: Vector2 = ball_sprite.scale;
 @onready var player_arrow = $PlayerArrow;
 @onready var player_arrow_animator = $PlayerArrow/AnimateArrow;
 @onready var particles = $GPUParticles2D;
+@onready var particles_dash = $DashParticles;
+@onready var particles_dash_landing = $DashLandingParticles;
 
 const FRICTION: int = 10;
 const GRAVITY: int = 9;
@@ -56,19 +63,31 @@ func _physics_process(delta):
 	mouse_position = get_global_mouse_position();
 	direction = global_position.direction_to(mouse_position);
 	ball_sprite.visible = false;
+	dash_sprite.visible = false;
+	dash_sprite_ball.visible = false;
 	sprite_eyes_body.position = sprite_eyes_body.position.lerp(sprite.position, delta * follow_speed);
 	sprite_eyes_ball.position = sprite_eyes_ball.position.lerp(ball_sprite.position, delta * follow_speed);
+	sprite_eyes_dash.position = sprite_eyes_dash.position.lerp(dash_sprite.position, delta * follow_speed);
+	sprite_eyes_dash_ball.position = sprite_eyes_dash_ball.position.lerp(dash_sprite_ball.position, delta * follow_speed);
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		hold_right_button = true;
-		ball_sprite.visible = true;
-		sprite.visible = false;
 		can_jump = false;
+		if !dash_mode:
+			ball_sprite.visible = true;
+			sprite.visible = false;
+		else:
+			dash_sprite_ball.visible = true;
+			dash_sprite.visible = false;
 	else:
 		hold_right_button = false;
-		ball_sprite.visible = false;
-		sprite.visible = true;
 		can_jump = true;
+		if !dash_mode:
+			ball_sprite.visible = false;
+			sprite.visible = true;
+		else:
+			dash_sprite_ball.visible = false;
+			dash_sprite.visible = true;
 	
 	scale_based_on_velocity();
 	
@@ -79,11 +98,17 @@ func _physics_process(delta):
 			if is_charging_jump:
 				current_jump_time += delta * 2;
 				current_jump_time = clamp(current_jump_time, JUMP_TIME_START, JUMP_TIME_MAX);
-				animation_player.play("charge_jump");
+				if !dash_mode:
+					animation_player.play("charge_jump");
+				else:
+					animation_player.play("charge_jump_dash");
 				player_arrow_animator.play("charge_arrow");
 		if Input.is_action_just_released("left_click"):
 			is_charging_jump = false;
-			animation_player.play("release_jump");
+			if !dash_mode:
+				animation_player.play("release_jump");
+			else:
+				animation_player.play("release_jump_dash");
 			player_arrow_animator.play("RESET");
 			jumping_movement();
 			player_arrow.visible = false;
@@ -109,6 +134,7 @@ func _physics_process(delta):
 			velocity.y += ADDITIONAL_GRAVITY;
 	if dash_mode and dash_count != 0:
 		if Input.is_action_just_pressed("hold_right_button"):
+			particles_dash.restart();
 			dash_movement();
 			dash_count = 0;
 	if is_on_floor() or is_on_wall():
@@ -128,18 +154,28 @@ func _physics_process(delta):
 		ball_sprite.flip_h = false;
 		sprite_eyes_body.flip_h = false;
 		sprite_eyes_ball.flip_h = false;
+		sprite_eyes_dash.flip_h = false;
+		sprite_eyes_dash_ball.flip_h = false;
 	elif velocity.x < 0:
 		sprite.flip_h = true;
 		ball_sprite.flip_h = true;
 		sprite_eyes_body.flip_h = true;
 		sprite_eyes_ball.flip_h = true;
+		sprite_eyes_dash.flip_h = true;
+		sprite_eyes_dash_ball.flip_h = true;
 	else:
 		pass;
 	
 	if is_on_floor():
 		if not on_ground:
-			animation_player.play("landing");
-			particles.restart();
+			if !dash_mode:
+				animation_player.play("landing");
+			else:
+				animation_player.play("landing_dash");
+			if !dash_mode:
+				particles.restart();
+			else:
+				particles_dash_landing.restart();
 		on_ground = true;
 	else:
 		on_ground = false;
@@ -165,8 +201,9 @@ func dash_movement() -> void:
 
 func apply_gravity() -> void:
 	if not is_on_floor():
-		velocity.y += GRAVITY;
-		velocity.y = min(velocity.y, 150);
+		velocity.y += 0.95*GRAVITY;
+		#velocity.y = min(velocity.y, 150); 
+		#Sprawia, że spadamy ze stałą prędkością, co uniemożliwia dashowania w dół.
 
 func apply_friction() -> void:
 	velocity.x = move_toward(velocity.x, 0, FRICTION);
@@ -193,7 +230,10 @@ func apply_wall_slide(delta) -> void:
 
 func change_to_dash():
 	dash_mode = true;
-
+	ball_sprite.visible = false;
+	sprite.visible = false;
+	dash_sprite.visible = true;
+	
 func rotate_player_arrow():
 	mouse_position = get_global_mouse_position();
 	player_arrow.look_at(mouse_position);
