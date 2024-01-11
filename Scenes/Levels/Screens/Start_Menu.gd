9ea4f94ae7e1635369d6c529dd357357c0075e18
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-11 22:26:10 +0100

    Added level selection

--- Scenes/Levels/Screens/Start_Menu.gd
+++ Scenes/Levels/Screens/Start_Menu.gd
@@ -8,7 +8,7 @@ func _ready():
 	$VBoxContainer.custom_minimum_size = Vector2(480, 270);
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/Level1/Level_1.tscn");
+	$AnimationPlayer.play("To_Levels");
 
 
 func _on_options_pressed():
@@ -17,3 +17,12 @@ func _on_options_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit();
+
+
+# Levels selection
+func _on_level_1_text_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level1/Level_1.tscn");
+
+
+func _on_level_2_text_pressed():
+	pass # Replace with function body.
