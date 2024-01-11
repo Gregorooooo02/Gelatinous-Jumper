f8969fa23537f091e971766bd1c5c3a294f83408
Wiktoryk <242486@edu.p.lodz.pl>
2024-01-11 01:12:02 +0100

    Revert "dzięki git"
    This reverts commit eaaed63c7d562e54c775c08435aae5efac9ecb60.

--- Scenes/Levels/Screens/end_box.gd
+++ Scenes/Levels/Screens/end_box.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
 func _on_body_entered(body):
-	if body is Player:
-		get_tree().change_scene_to_file("res://Scenes/Levels/Screens/End_Screen.tscn");
-		DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE);
+	get_tree().change_scene_to_file("res://Scenes/Levels/Screens/End_Screen.tscn");
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_VISIBLE);
