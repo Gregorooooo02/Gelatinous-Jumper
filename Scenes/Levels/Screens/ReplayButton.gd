e2d0b62ead42db0dc7a93564257d109d6bf310bb
Wiktoryk <242486@edu.p.lodz.pl>
2024-01-11 16:59:24 +0100

    wszystkie sekcje,bez funkcjonalności

--- Scenes/Levels/Screens/ReplayButton.gd
+++ Scenes/Levels/Screens/ReplayButton.gd
@@ -2,4 +2,4 @@ extends Button;
 
 func _on_pressed() -> void :
 	get_node("../../Main").timer = 0
-	get_tree().change_scene_to_file("res://Scenes/Levels/Level1/Level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/Level2/Level_2.tscn")
