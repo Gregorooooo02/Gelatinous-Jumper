e172e899f21575b2b4a3c6c27b63d272562a8487
Wiktoryk <242486@edu.p.lodz.pl>
2024-01-23 21:08:42 +0100

    merge fix

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,6 +6,7 @@ class_name World;
 
 var timer = 0
 var should_start = false;
+var coins = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,11 +28,19 @@ func update_timer_display():
 	var time_label = get_node("../World/Camera2D/TimerDisplay")
 	if time_label:
 		time_label.text = str(int(timer)) + ":" + str(int((timer-int(timer))*1000))
-		
-#func format_time(seconds):
-	#var minutes = seconds // 60
-	#var remaining_seconds = seconds % 60
-	#return String().format("%02d:%02d", minutes, remaining_seconds)
+
+func reset_coins():
+	coins = 0
+	update_coins_display()
+
+func add_coins(amount):
+	coins += amount
+	update_coins_display()
+
+func update_coins_display():
+	var coin_label = get_node("../World/Camera2D/CoinDisplay")
+	if coin_label:
+		coin_label.text = str(coins*10)
 
 func _on_timer_timeout():
 	pass # Replace with function body.
