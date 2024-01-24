740504f4e94f7a12e3237c0929ffcf3275cf4a46
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-24 18:21:31 +0100

    Making menu transition a little bit nicer

---
+++ Main.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var transition_player = $CutOutAnimation/AnimationPlayer;
+@onready var menu_transition = $MainMenu/AnimationPlayer;
+@onready var transition_sound = $MainMenu/TransitionSFX;
+
+func _ready():
+	$CutOutAnimation.visible = true;
+	$CutOutAnimation/AnimationPlayer.play("FadeFromBlack");
+	menu_transition.stop();
+
+
+func _on_animation_player_animation_finished(anim_name):
+	menu_transition.play("Start");
