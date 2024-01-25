b11a7802e6f9448ddeb390bdb70f578db5764efc
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-25 03:12:53 +0100

    Coin is cool

--- Scenes/Items/coin.gd
+++ Scenes/Items/coin.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @onready var sprite_animation = $Sprite2D;
+@onready var collision = $Area2D/CollisionShape2D;
+@onready var area = $Area2D;
+@onready var particles = $CPUParticles2D;
+@onready var timer = $Timer;
 
 func _ready():
 	sprite_animation.play("default");
@@ -9,4 +13,11 @@ func _ready():
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		get_node("/root/Main").add_coins(1)
-		queue_free();
+		sprite_animation.visible = false;
+		collision.disabled = true;
+		particles.emitting = true;
+		timer.start();
+
+
+func _on_timer_timeout():
+	queue_free();
