63a90da0a16fd31d92ce64515d62c384fe35f73d
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-30 12:21:51 +0100

    Deleted options, because we have none

--- Scenes/Levels/Screens/Start_Menu.gd
+++ Scenes/Levels/Screens/Start_Menu.gd
@@ -17,13 +17,6 @@ func _on_start_pressed():
 	confirm_sound.play();
 	$AnimationPlayer.play("To_Levels");
 
-
-func _on_options_pressed():
-	confirm_sound.play();
-	print("Options pressed");
-	
-
-
 func _on_exit_pressed():
 	confirm_sound.play();
 	get_tree().quit();
