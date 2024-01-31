60230c886d1a4022a9cc5fccd072e3b2a3532ef1
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-01-31 22:51:57 +0100

    Inputs with middle click for powerups

--- Scenes/Items/Dash_PowerUp.gd
+++ Scenes/Items/Dash_PowerUp.gd
@@ -8,9 +8,10 @@ var can_change = false;
 
 func _ready():
 	animation_player.play("Basic");
+	$InputSprite.play("default");
 
 func _process(delta):
-	if can_change and Input.is_action_just_pressed("space"):
+	if can_change and Input.is_action_just_pressed("middle_click"):
 		_change(Global.player);
 		can_change = false;
 
